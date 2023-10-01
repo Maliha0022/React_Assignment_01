9ab7551a85cf48083bbd3603169256985fec7c51
Maliha <maliha.1601113@gmail.com>
2023-10-01 22:20:12 +0500

    performed empty task handling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 import './App.css';
+import '../src/components/UI/task.css'
+import React,{useState} from 'react';
+import Listtask from './components/ToDo_App/Listtask';
+import Addtask from './components/ToDo_App/Addtask';
 
 function App() {
+
+  const [Listtodo, setListTodo] = useState([]);
+  let addList = (inputText)=>{
+    if(inputText !== ''){
+      setListTodo([...Listtodo,inputText])
+    }
+  } 
+
+  const deleteListItem = (key) => {
+    let newListTodo = [...Listtodo];
+    newListTodo.splice(key,1)
+    setListTodo([...newListTodo])
+  }
+
   return (
     <>
     
-      <h1>Hello world</h1>
+      <div className='main-container'>
+        <div className='center-container'> 
+
+        <Addtask addList={addList}/>
+        <h1 className="app-heading">TODO</h1>
+        <hr/>
+        {Listtodo.map((listItem,i)=>{
+          return (
+            <Listtask key={i} index={i} item={listItem} deleteItem={deleteListItem}/>
+          )
+        })}
+      
+
+        </div>
+      </div>
 
     </>
   );
